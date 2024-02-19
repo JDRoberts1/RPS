52e9bbca78148bdf8c11608132058244c2c2c687
Jeanai Roberts <58571944+JDRoberts1@users.noreply.github.com>
2024-02-19 17:20:11 -0600

    added files

---
+++ RPS/RPS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Nai Code on 2/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var newGameButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func startGame(){
+        let vc = (storyboard?.instantiateViewController(withIdentifier: "GameViewController"))!
+        self.present(vc, animated: true, completion: nil)
+    }
+
+
+}
+
